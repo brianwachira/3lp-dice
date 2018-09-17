d306a13a4936976a7a20536ce906737ec3b25d0b
briananointed <brianwachira7@gmail.com>
2018-09-17 15:18:45 +0300

    add buildupscore property in pigDice constructor,add playgame prototype,pigRandom() function, set global variables inside document.ready(function()), set it to create instances onclick inside startGame function,add if else functionality to deal with score inside roll.click function

--- js/scripts.js
+++ js/scripts.js
@@ -3,24 +3,59 @@
 function pigDice()  {
   this.player;
   this.score;
+  this.buildingUpScore;
 }
 pigDice.prototype.startGame = function()  {
   this.score = 0;
-  console.log("game started succesfully");
+  this.buildingUpScore = 0;
+}
+pigDice.prototype.playGame = function() {
+  var diceSide = pigRandom();
+
+  if (diceSide != 1) {
+   this.score +=diceSide;
+   this.buildingUpScore +=diceSide;
+}else {
+  alert("score goes back to 0");
+  diceSide = 0;
+  this.score -=this.buildingUpScore;
+  this.buildingUpScore = 0;
+}
+return diceSide;
 }
 pigDice.prototype.endGame = function()  {
 
 }
 
+function pigRandom()  {
+  return Math.round(Math.random() * 6);
+}
 //ui logic
 $(document).ready(function(){
+  var player1;
+  var player2;
+  var player1hasnotplayed = new Boolean(true);
+  var DiceRollResult = 0;
   $(".startGame").click(function(event){
     $(".game-showing").fadeToggle();
+    player1 = new pigDice();
+    player1.startGame();
+    player2 = new pigDice();
+    player2.startGame();
     event.preventDefault();
   });
 
   $("#roll").click(function(event){
-    var player1 = new pigDice();
-    player1.startGame();
+    if (player1hasnotplayed) {
+      DiceRollResult = player1.playGame();
+      $("#player1-roll").text(DiceRollResult);
+      $("#player1-score").text(player1.score);
+      player1hasnotplayed = false;
+    }else {
+      DiceRollResult = player2.playGame();
+      $("#player2-roll").text(DiceRollResult);
+      $("#player2-score").text(player2.score);
+      player1hasnotplayed = true;
+    }
   });
 });
