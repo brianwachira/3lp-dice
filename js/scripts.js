0315a8d4f5295e7a0e0b61cd6da8f13d38d2bf08
briananointed <brianwachira7@gmail.com>
2018-09-17 13:23:13 +0300

    add business logic that sets game

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+//business logic
+
+function pigDice()  {
+  this.player;
+  this.score;
+}
+pigDice.prototype.startGame = function()  {
+  this.score = 0;
+}
+pigDice.prototype.endGame = function()  {
+
+}
